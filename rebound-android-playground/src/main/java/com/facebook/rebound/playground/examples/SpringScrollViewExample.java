60c5178adffb15ac5353fe2661ad2cf2d5fd987b
Pedro Paulo de Amorim <pp.amorim@hotmail.com>
2015-06-28 02:21:06 -0300

    uncommited change

--- rebound-android-playground/src/main/java/com/facebook/rebound/playground/examples/SpringScrollViewExample.java
+++ rebound-android-playground/src/main/java/com/facebook/rebound/playground/examples/SpringScrollViewExample.java
@@ -24,25 +24,24 @@ import com.facebook.rebound.playground.examples.scrollview.ExampleRowView;
 
 public class SpringScrollViewExample extends FrameLayout {
 
-  private final int ROW_COUNT = 50;
+  private static final String TEXT = "Row ";
+  private static final int ROW_COUNT = 50;
 
   public SpringScrollViewExample(Context context) {
     super(context);
-
     LayoutInflater inflater = LayoutInflater.from(context);
     ViewGroup root = (ViewGroup) inflater.inflate(R.layout.spring_scroll_view_example, this, false);
     ViewGroup content = (ViewGroup) root.findViewById(R.id.content_view);
     addView(root);
     setBackgroundColor(Color.argb(255, 50, 50, 50));
-
     int startColor = Color.argb(255, 255, 64, 230);
     int endColor = Color.argb(255, 0, 174, 255);
     ArgbEvaluator evaluator = new ArgbEvaluator();
     for (int i = 0; i < ROW_COUNT; i++) {
       ExampleRowView exampleRowView = new ExampleRowView(context);
-      Integer color = (Integer) evaluator.evaluate((float) i / (float) ROW_COUNT, startColor, endColor);
-      exampleRowView.setText("Row " + i);
-      exampleRowView.setBackgroundColor(color);
+      exampleRowView.setText(TEXT + i);
+      exampleRowView.setBackgroundColor((Integer) evaluator.evaluate(
+          (float) i / (float) ROW_COUNT, startColor, endColor));
       content.addView(exampleRowView);
     }
   }
