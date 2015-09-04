6f47584236344246fe19e3c5ae6ff8b21d84e399
ppamorim <pp.amorim@hotmail.com>
2015-09-04 14:29:12 -0300

    licenses added and minor changes

--- rebound-android/src/main/java/com/facebook/rebound/HardwareLayerSpringListener.java
+++ rebound-android/src/main/java/com/facebook/rebound/HardwareLayerSpringListener.java
@@ -1,9 +1,23 @@
+/*
+ *  Copyright (c) 2015, Facebook, Inc.
+ *  All rights reserved.
+ *
+ *  This source code is licensed under the BSD-style license found in the
+ *  LICENSE file in the root directory of this source tree. An additional grant
+ *  of patent rights can be found in the PATENTS file in the same directory.
+ *
+ */
+
 package com.facebook.rebound;
 
 import android.view.View;
 import java.util.ArrayList;
 import java.util.WeakHashMap;
 
+/**
+ * Spring Listener with Hardware Layer implementation, it's useful if
+ * you use with complex transition in your views
+ */
 public class HardwareLayerSpringListener implements SpringListener {
 
   /**
