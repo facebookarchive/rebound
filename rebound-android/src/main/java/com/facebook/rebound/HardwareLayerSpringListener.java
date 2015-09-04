311511b117ed9f8acb6c0f051dd132ea69248b15
ppamorim <pp.amorim@hotmail.com>
2015-09-04 14:26:00 -0300

    update

--- rebound-android/src/main/java/com/facebook/rebound/HardwareLayerSpringListener.java
+++ rebound-android/src/main/java/com/facebook/rebound/HardwareLayerSpringListener.java
@@ -1,27 +1,58 @@
 package com.facebook.rebound;
 
 import android.view.View;
+import java.util.ArrayList;
 import java.util.WeakHashMap;
 
 public class HardwareLayerSpringListener implements SpringListener {
 
+  /**
+   * I think that WeakHashMap can avoid any memory leak, needs to be tested...
+   */
   private WeakHashMap<Integer, View> weakViews = new WeakHashMap<>();
 
   public HardwareLayerSpringListener(View... views) {
-    int countViews = views.length;
-    for(int i = 0; i < countViews; i++) {
-      weakViews.put(i, views[i]);
-    }
+    addViews(views);
   }
 
   public HardwareLayerSpringListener(WeakHashMap<Integer, View> weakViews) {
     this.weakViews = weakViews;
   }
 
+  public void addViews(ArrayList<View> views) {
+    int countViews = views.size();
+    for(int i = 0; i < countViews; i++) {
+      addView(i, views.get(i));
+    }
+  }
+
+  public void addViews(WeakHashMap<Integer, View> weakViews) {
+    int countViews = weakViews.size();
+    for(int i = 0; i < countViews; i++) {
+      addView(i, weakViews.get(i));
+    }
+  }
+
+  public void addViews(View... views) {
+    int countViews = views.length;
+    for(int i = 0; i < countViews; i++) {
+      addView(i, views[i]);
+    }
+  }
+
+  public void addView(int index, View view) {
+    weakViews.put(index, view);
+  }
+
   @Override
   public void onSpringUpdate(Spring spring) {
   }
 
+  /**
+   * Removes the all views of the Hardware Layer
+   *
+   * @param spring the spring that's now resting
+   */
   @Override
   public void onSpringAtRest(Spring spring) {
     for(View view : weakViews.values()) {
@@ -29,6 +60,11 @@ public class HardwareLayerSpringListener implements SpringListener {
     }
   }
 
+  /**
+   * Set the layer type of the all views to View.LAYER_TYPE_HARDWARE
+   *
+   * @param spring the spring that has left its resting state
+   */
   @Override
   public void onSpringActivate(Spring spring) {
     for(View view : weakViews.values()) {
